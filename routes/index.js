cdfc987859dedd1534cdf5666e3b6c4165708112
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-12-05 15:44:47 +0300

    Add files via upload
    Signed-off-by: HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,14 @@
-// Import the required modules
-const express = require('express');
+import { Router } from 'express';
+import appRouter from './app';
+import authRouter from './auth';
+import usersRouter from './users';
+import filesRouter from './files';
 
-// Import the controllers
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
+// App router
+const router = Router();
+router.use(appRouter);
+router.use(authRouter);
+router.use(usersRouter);
+router.use(filesRouter);
 
-// Create the router
-const router = express.Router();
-
-// Define the routes
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getStats);
-router.get('/users/me ', UsersController.getStats);
-// Define the route for creating a new user
-router.post('/users', UsersController.postNew);
-
-// Export the router
-module.exports = router;
+export default router;
